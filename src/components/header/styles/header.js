f574bb4a7c6c4edfbe2e4d2166a41569c7e4dcf8
chetanchandel31 <chetanchandel3@gmail.com>
2021-05-22 22:12:31 +0530

    add browse page header

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -1,10 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link as ReactRouterLink } from "react-router-dom";
 import styled from "styled-components/macro";
 
 export const Background = styled.div`
 	display: flex;
 	flex-direction: column;
 	background: url(${({ src }) => (src ? `../images/misc/${src}.jpg` : "../images/misc/home-bg.jpg")}) top left / cover no-repeat;
+
+	@media (max-width: 1100px) {
+		${({ dontShowOnSmallViewPort }) => dontShowOnSmallViewPort && `background: none;`}
+	}
+`;
+
+export const Group = styled.div`
+	display: flex;
+	align-items: center;
 `;
 
 export const Container = styled.div`
@@ -24,6 +33,41 @@ export const Container = styled.div`
 	}
 `;
 
+export const Picture = styled.button`
+	background: url(${({ src }) => src});
+	background-size: contain;
+	border: 0;
+	width: 32px;
+	height: 32px;
+	cursor: pointer;
+`;
+
+export const Dropdown = styled.div`
+	display: none;
+	background-color: black;
+	position: absolute;
+	padding: 10px;
+	width: 100px;
+	top: 32px;
+	right: 10px;
+`;
+
+export const Profile = styled.div`
+	display: flex;
+	align-items: center;
+	margin-left: 20px;
+	position: relative;
+
+	button {
+		cursor: pointer;
+	}
+
+	&:hover > ${Dropdown} {
+		display: flex;
+		flex-direction: column;
+	}
+`;
+
 export const Logo = styled.img`
 	height: 32px;
 	width: 108px;
@@ -35,7 +79,7 @@ export const Logo = styled.img`
 	}
 `;
 
-export const ButtonLink = styled(Link)`
+export const ButtonLink = styled(ReactRouterLink)`
 	display: block;
 	background-color: #e50914;
 	width: 84px;
@@ -52,4 +96,49 @@ export const ButtonLink = styled(Link)`
 	&:hover {
 		background-color: #f40612;
 	}
+`;
+
+export const Feature = styled(Container)`
+	padding: 150px 0 500px 0;
+	flex-direction: column;
+	align-items: normal;
+	width: 50%;
+
+	@media (max-width: 1100px) {
+		display: none;
+	}
+`;
+
+export const Text = styled.p`
+	color: white;
+	font-weight: 22px;
+	line-height: normal;
+	text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
+	margin: 0;
+`;
+
+export const Link = styled.p`
+	color: white;
+	text-decoration: none;
+	margin-right: 30px;
+	font-weight: ${({ active }) => (active === "true" ? "700" : "normal")};
+	cursor: pointer;
+
+	&:hover {
+		font-weight: bold;
+	}
+
+	&:last-of-type {
+		margin-right: 0;
+	}
+`;
+
+export const FeatureCallOut = styled.h1`
+	color: white;
+	font-weight: 50px;
+	line-height: normal;
+	font-weight: bold;
+	text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
+	margin: 0;
+	margin-bottom: 20px;
 `;
--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,7 +1,10 @@
 import { useContext, useEffect, useState } from "react";
 import { FirebaseContext } from "../context/firebase";
 import { SelectProfileContainer } from "./profile";
-import { Loading } from "../components/";
+import { Header, Loading } from "../components/";
+import { ReleaseBody } from "../components/loading/styles/loading";
+import * as ROUTES from "../constants/routes";
+import logo from "../logo.svg";
 
 export function BrowseContainer({ slides }) {
 	const [profile, setProfile] = useState({});
@@ -10,12 +13,46 @@ export function BrowseContainer({ slides }) {
 	const user = firebase.auth().currentUser || {};
 
 	useEffect(() => {
-		console.log(profile, "profile");
+		// console.log(profile, "profile");
 		setTimeout(() => setLoading(false), 3000);
 		//eslint-disable-next-line
 	}, [profile.displayName]);
 
-	// prettier-ignore
-	//make it if/else
-	return profile.displayName ? (loading ? <Loading src={user.photoURL} /> : null) : <SelectProfileContainer user={user} setProfile={setProfile} />;
+	//make it if-else
+	return profile.displayName ? (
+		<>
+			{loading ? <Loading src={user.photoURL} /> : <ReleaseBody />}
+			<Header src="joker1" dontShowOnSmallViewPort>
+				<Header.Frame>
+					<Header.Group>
+						<Header.Logo to={ROUTES.HOME} src={logo} alt="Jetflix" />
+						<Header.TextLink>Series</Header.TextLink>
+						<Header.TextLink>Films</Header.TextLink>
+					</Header.Group>
+					<Header.Group>
+						<Header.Profile>
+							<Header.Picture src={user.photoURL} />
+							<Header.Dropdown>
+								<Header.Group>
+									<Header.Picture src={user.photoURL} />
+									<Header.TextLink>{user.displayName}</Header.TextLink>
+								</Header.Group>
+							</Header.Dropdown>
+						</Header.Profile>
+					</Header.Group>
+				</Header.Frame>
+				<Header.Feature>
+					<Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+					<Header.Text>
+						Firebase is an incredible platform, some of their recent additions like Cloud Functions for static hosting are pretty amazing. However, I’d like to
+						share a serious blindspot that I came across while preparing for a product launch. The intent of this post is in no way to discourage anyone from
+						using Firebase. I love Firebase and I recommend you give it a go! This is just an important learning that I wanted to share — hopefully it can
+						prevent you from getting stuck in the same position as me.
+					</Header.Text>
+				</Header.Feature>
+			</Header>
+		</>
+	) : (
+		<SelectProfileContainer user={user} setProfile={setProfile} />
+	);
 }
